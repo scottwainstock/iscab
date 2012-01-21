ba825af62e30bb88b51259b1878fa89a9f84278b
Scott Wainstock <scott.wainstock@gmail.com>
2012-01-21 13:25:22 -0800

    add back rotation notifications

--- iscab/GamePlay.m
+++ iscab/GamePlay.m
@@ -84,6 +84,7 @@ AppDelegate *app;
 }
 
 - (void)didRotate {
+    NSLog(@"ROTATED");
     if (([[UIDevice currentDevice] orientation] == UIDeviceOrientationLandscapeLeft))
         space->gravity = ccp(-GRAVITY_FACTOR, 0);
     else if (([[UIDevice currentDevice] orientation] == UIDeviceOrientationLandscapeRight))
@@ -91,7 +92,7 @@ AppDelegate *app;
     else if (([[UIDevice currentDevice] orientation] == UIDeviceOrientationPortrait))
         space->gravity = ccp(0, -GRAVITY_FACTOR);
     else if (([[UIDevice currentDevice] orientation] == UIDeviceOrientationPortraitUpsideDown))
-        space->gravity = ccp(0, GRAVITY_FACTOR);
+        space->gravity = ccp(0, -GRAVITY_FACTOR);
 
     self.gravity = space->gravity;
 }
@@ -105,10 +106,7 @@ AppDelegate *app;
         
         [self setupSkinBackgroundBoundaries];
         [self createSpace];
-        
-        //[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didRotate) name:UIDeviceOrientationDidChangeNotification object:nil];
-         
-        
+                 
         [[UIApplication sharedApplication] cancelAllLocalNotifications];
         [self scheduleUpdate];
         
@@ -319,6 +317,8 @@ AppDelegate *app;
     NSLog(@"GAMEPLAY ONENTER");
     [super onEnter];
     
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didRotate) name:UIDeviceOrientationDidChangeNotification object:nil];
+    
     [self addChild:app.batchNode];
     
     if (![app.defaults objectForKey:@"scab"]) {
@@ -333,6 +333,9 @@ AppDelegate *app;
 
 - (void)onExit {
     NSLog(@"GAMEPLAY ON EXIT");
+
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIDeviceOrientationDidChangeNotification object:nil];
+
     [app scheduleNotifications];
     [app cleanupBatchNode];
     [app saveState];
